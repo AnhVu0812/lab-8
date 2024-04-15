85705cd616dcfc3f4fea40969dbbaea3656e2c93
AnhVu0812 <141117873+AnhVu0812@users.noreply.github.com>
2024-04-15 23:11:56 +0700

    add index,js

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
 var doPrime = document.getElementById("songuyen")
 
-const prime = function(n) {
+const prime = function(n) 
+{
     var n = doPrime.value;
     var flag = true;
-    if (n < 2) {
+
+    if (n < 2) 
+    {
         flag = false;
     }
 
     else {
-        for (let i = 2; i <= Math.sqrt(n); i++){
-            if (n % 2 == 0){
+        for (let i = 2; i <= Math.sqrt(n); i++)
+        {
+            if (n % 2 == 0)
+            {
                 flag = false;
                 break
             }
-            else {
+
+            else 
+            {
                 flag = true;
-                break
             }
         }
     }
-    if (flag == true){
+    
+    if (flag == true)
+    {
         document.getElementById("result").innerHTML = `${n} is prime <br/>`;
     }
-    else {
+
+    else 
+    {
         document.getElementById("result").innerHTML = `${n} is not prime <br/>`;
     }
 }
-
